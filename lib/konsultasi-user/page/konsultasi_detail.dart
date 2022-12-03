f39d8443d9fca605568e8672406eeadf3be1408b
fahiradh <fahiraadindiah16@gmail.com>
2022-12-03 09:53:44 +0700

    minus showing form page in main page

---
+++ lib/konsultasi-user/page/konsultasi_detail.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'package:proyek_tugas_akhir/konsultasi-user/page/form.dart';
+
+class DetailKonsultasi extends StatefulWidget {
+  const DetailKonsultasi({
+    super.key,
+    required this.title,
+    required this.user,
+    required this.date,
+    required this.name,
+    required this.description,
+  });
+
+  final String title;
+  final int user;
+  final DateTime date;
+  final String name;
+  final String description;
+
+  @override
+  State<DetailKonsultasi> createState() => _DetailKonsultasiState();
+}
+
+class _DetailKonsultasiState extends State<DetailKonsultasi> {
+  static const primaryColor = Color(0xFF2D55D0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text(''),
+            backgroundColor: primaryColor,
+            leadingWidth: 64,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back),
+              //replace with our own icon data.
+            )),
+        // Menambahkan drawer menu
+        body: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.title,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Text(
+                  DateFormat.yMMMMd().format(widget.date),
+                ),
+              ),
+              Text(widget.name),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Row(children: [
+                  Flexible(
+                    child: Text(widget.description),
+                  )
+                ]),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Align(
+          alignment: Alignment.bottomRight,
+          child: FloatingActionButton(
+            backgroundColor: primaryColor,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MyFormPage()),
+              );
+            },
+            child: const Icon(Icons.reply_rounded),
+          ),
+        ));
+  }
+}
