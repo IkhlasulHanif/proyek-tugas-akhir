16c0032c54fe57c6eaa1899b534d16e48d0f1108
Ikhlasul Akmal Hanif <2112akmal@gmail.com>
2022-12-12 21:17:49 +0700

    push post

--- lib/home & login/page/loginpage.dart
+++ lib/home & login/page/loginpage.dart
@@ -20,6 +20,13 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _loginFormKey = GlobalKey<FormState>();
 
+  bool isPasswordVisible = true;
+  void togglePasswordView() {
+    setState(() {
+      isPasswordVisible = !isPasswordVisible;
+    });
+  }
+
   String username = "";
   String password1 = "";
 
@@ -28,104 +35,139 @@ class _LoginPageState extends State<LoginPage> {
     final request = context.watch<CookieRequest>();
     final user = context.watch<UserManagement>();
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login Page'),
-          backgroundColor: Color(0xFF548AFF),
-        ),
-        body: Form(
-            key: _loginFormKey,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Username',
-                        hintText: 'Enter your username',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0))),
-                    onChanged: (String? value) {
-                      setState(() {
-                        username = value!;
-                      });
-                    },
-                    onSaved: (String? value) {
-                      setState(() {
-                        username = value!;
-                      });
-                    },
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Username tidak boleh kosong!';
-                      }
-                      return null;
-                    },
+      appBar: AppBar(title: const Text('Login Page')),
+      body: SingleChildScrollView(
+          child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('assets/LOGO.png',
+                        width: MediaQuery.of(context).size.width / 2.5),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        labelText: 'Password',
-                        hintText: 'Enter your password',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0))),
-                    onChanged: (String? value) {
-                      setState(() {
-                        password1 = value!;
-                      });
-                    },
-                    onSaved: (String? value) {
-                      setState(() {
-                        password1 = value!;
-                      });
-                    },
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password tidak boleh kosong!';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    if (_loginFormKey.currentState!.validate()) {
-                      try {
-                        final response = await request
-                            .login("http://127.0.0.1:8000/user-details/", {
-                          'username': username,
-                          'password': password1,
-                        });
-                        if (request.loggedIn) {
-                          var loginuser = UserLogin.fromJson(response["data"]);
-                          user.setUser(loginuser);
-                          print(loginuser.username);
-                          print(loginuser.is_konsulir);
-                          if (loginuser.is_konsulir) {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: ((context) => const AdminPage(
-                                          title: '',
-                                        ))));
-                          } else {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: ((context) =>
-                                        const ReportSummary())));
-                          }
-                        }
-                      } catch (_) {
-                        print("error");
-                      }
-                    }
-                  },
-                  child: const Text('Login'),
-                ),
-              ],
-            )));
+                  Form(
+                      key: _loginFormKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  icon: const Icon(Icons.person),
+                                  labelText: 'Username',
+                                  hintText: 'Enter your username',
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(5.0))),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  username = value!;
+                                });
+                              },
+                              onSaved: (String? value) {
+                                setState(() {
+                                  username = value!;
+                                });
+                              },
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Username tidak boleh kosong!';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              obscureText: isPasswordVisible,
+                              decoration: InputDecoration(
+                                  icon: const Icon(Icons.lock_person_outlined),
+                                  labelText: 'Password',
+                                  hintText: 'Enter your password',
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0)),
+                                  suffixIcon: Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(0, 0, 4, 0),
+                                    child: InkWell(
+                                      onTap: togglePasswordView,
+                                      child: Icon(
+                                        isPasswordVisible
+                                            ? Icons.visibility_rounded
+                                            : Icons.visibility_off_rounded,
+                                        size: 24,
+                                      ),
+                                    ),
+                                  )),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  password1 = value!;
+                                });
+                              },
+                              onSaved: (String? value) {
+                                setState(() {
+                                  password1 = value!;
+                                });
+                              },
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Password tidak boleh kosong!';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () async {
+                              if (_loginFormKey.currentState!.validate()) {
+                                try {
+                                  final response = await request.login(
+                                      "https://web-production-c284.up.railway.app/user-details/",
+                                      {
+                                        'username': username,
+                                        'password': password1,
+                                      });
+                                  if (request.loggedIn) {
+                                    var loginuser =
+                                        UserLogin.fromJson(response["data"]);
+                                    user.setUser(loginuser);
+                                    print(loginuser.username);
+                                    print(loginuser.is_konsulir);
+                                    if (loginuser.is_konsulir)
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: ((context) =>
+                                                  const AdminPage(
+                                                    title: '',
+                                                  ))));
+                                    else
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: ((context) =>
+                                                  const UserPage(
+                                                    title: '',
+                                                  ))));
+                                  }
+                                } catch (_) {
+                                  print("error");
+                                }
+                              }
+                            },
+                            child: const Text('Login'),
+                          ),
+                        ],
+                      ))
+                ],
+              ))),
+    );
   }
 }
